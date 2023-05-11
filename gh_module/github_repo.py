84ddad437c6fbe53d4ec007ca308f0c7bab060ed
Brian Stephens <bkis1969@gmail.com>
2023-05-11 18:45:48 +0000

    fields

--- gh_module/github_repo.py
+++ gh_module/github_repo.py
@@ -11,3 +11,20 @@ def main():
 
 if __name__ == '__main__':
     main()
+def main():
+
+    fields = {
+        "github_auth_key": {"required": True, "type": "str"},
+        "username": {"required": True, "type": "str"},
+        "name": {"required": True, "type": "str"},
+        "description": {"required": False, "type": "str"},
+        "private": {"default": False, "type": "bool"},
+        "has_issues": {"default": True, "type": "bool"},
+        "has_wiki": {"default": True, "type": "bool"},
+        "has_downloads": {"default": True, "type": "bool"},
+        "state": {
+            "default": "present",
+            "choices": ['present', 'absent'],
+            "type": 'str'
+        },
+    }
